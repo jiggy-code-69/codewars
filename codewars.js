5377f6061ddc8af4cb60a795c97a717b52ea4081
jiggy-code-69 <121889884+jiggy-code-69@users.noreply.github.com>
2023-04-20 22:22:22 +0530

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -60,3 +60,44 @@ function longest(s1, s2) {
     let Str =Array.from(new Set(newArr))
     return Str.sort().join("");
 }
+
+
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+// You need to round the answer to 2 decimal places and return it as String.
+
+// If the given value is 0 then it should return 0.00
+
+// You will only be given Natural Numbers as arguments.
+
+// Examples:(Input -->1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+function SeriesSum(n) {
+  let sum = 0;
+  let denominator = 1;
+
+  for (let i = 0; i < n; i++) {
+    sum += 1 / denominator;
+    denominator += 3;
+  }
+
+  return sum.toFixed(2);
+}
+
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+// Examples (Input --> Output)
+// "1234"   -->  true
+// "12345"  -->  false
+// "a234"   -->  false
+
+
+function validatePIN(pin) {
+  return /^(\d{4}|\d{6})$/.test(pin);
+}
