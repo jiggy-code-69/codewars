c450ee73ab9d3f6654b73f2a0a9ac831bb92d500
jiggy-code-69 <121889884+jiggy-code-69@users.noreply.github.com>
2023-04-27 22:19:15 +0530

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -137,3 +137,25 @@ function XO(str) {
       const yCount = matches2 === null ? 0 : matches2.length;
   return xCount === yCount;
 }
+
+
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+// Mind the input validation.
+
+// Example
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+// Input validation
+// If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+function sumArray(arr) {
+  if (!Array.isArray(arr) || arr.length <= 1) {
+    return 0;
+  }
+  const sortedArr = arr.sort((a, b) => a - b);
+  const sum = sortedArr.slice(1, -1).reduce((acc, val) => acc + val, 0);
+  return sum;
+}
