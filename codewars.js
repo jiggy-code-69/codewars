92798f503f05271bef96c8097a00e32f9c084f6c
jiggy-code-69 <121889884+jiggy-code-69@users.noreply.github.com>
2023-06-21 22:02:32 +0530

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -159,3 +159,32 @@ function sumArray(arr) {
   const sum = sortedArr.slice(1, -1).reduce((acc, val) => acc + val, 0);
   return sum;
 }
+
+
+
+
+
+
+
+
+
+// ask
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+// Don't change the order of the elements that are left.
+
+// Examples
+// * Input: [1,2,3,4,5], output = [2,3,4,5]
+// * Input: [5,3,2,1,4], output = [5,3,2,4]
+// * Input: [2,2,1,2,1], output = [2,2,2,1]
+
+
+function removeSmallest(numbers) {
+if(numbers.length === 0){
+return []
+}
+let indexOfMin = numbers.indexOf(Math.min(...numbers));
+  
+let newArr = [...numbers.slice(0,indexOfMin),...numbers.slice(indexOfMin + 1)];
+return newArr;
+}
